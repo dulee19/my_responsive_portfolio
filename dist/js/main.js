8636c11d346d99527e0c640523fba07587c21957
dulee19 <adusan53@gmail.com>
2019-09-25 21:58:55 +0200

    Menu button, javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// DOM Items
+const menuBtn = document.querySelector(".menu-btn"),
+  menu = document.querySelector(".menu"),
+  menuNav = document.querySelector(".menu-nav"),
+  menuBranding = document.querySelector(".menu-branding"),
+  navItems = document.querySelectorAll(".nav-item");
+
+//   Set Inital State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
